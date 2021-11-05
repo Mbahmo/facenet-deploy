d30ff4f3417221afd2fb1b68e647b94e7e3e52fd
Anthony Lee <anthonyleembahmo@gmail.com>
2021-11-06 02:09:35 +0800

    Test Heroku #6

--- app.py
+++ app.py
@@ -126,5 +126,4 @@ def classify_route():
     return render_template('loading.html', url = url_for('index'))
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
